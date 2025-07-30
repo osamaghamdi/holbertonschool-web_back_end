5da39d25e95f58907f1b38eeaf1637f4ad1bc1db
Your Name <you@example.com>
2025-07-30 19:46:53 +0300

    create

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Computes the sum of a list of floats."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
-    """Returns the sum of a list of floats."""
-    return sum(input_list)
+def sum_list(input_list: List[float]) -> float:
+    """defining list of float"""
+    return float(sum(input_list))
