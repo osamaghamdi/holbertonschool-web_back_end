601134f9ee2ae713b0194ddd5157c05a9ce7cd55
Your Name <you@example.com>
2025-07-31 14:44:05 +0300

    create

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic asynchronous syntax example."""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Waits for a random delay between 0 and max_delay seconds and returns the delay."""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
