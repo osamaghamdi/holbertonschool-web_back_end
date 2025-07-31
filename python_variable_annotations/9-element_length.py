fcedfa650cc7113c00fbb0754461d057431569b8
Your Name <you@example.com>
2025-07-31 12:33:43 +0300

    create

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Returns the length of each element in a list of sequences."""
-from typing import Iterable, Tuple, Sequence
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> Iterable[Tuple[int, int]]:
-    """Returns a list of tuples containing the element and its length."""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples containing each element and its length."""
     return [(i, len(i)) for i in lst]
