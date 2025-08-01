bcfcb5895026efb5c076798343abbfb08ac107dc
Your Name <you@example.com>
2025-08-01 18:59:38 +0300

    create

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measure the time it takes to run async_comprehension four times concurrently"""
+    """Measure the time it takes to run async_comprehension."""
     start = time()
     await asyncio.gather(async_comprehension(), async_comprehension(),
                          async_comprehension(), async_comprehension())
